5b57a39ec1b7b46c279233085f48bb9fda9dc04c
hongbi-lee <foxgirin950@gmail.com>
2022-03-18 11:33:18 +0900

    week 2 lecture contents

--- dwu-oop/powerJava/src/ch05/CarArrayTest.java
+++ dwu-oop/powerJava/src/ch05/CarArrayTest.java
@@ -1,2 +1,29 @@
-package ch05;public class CarArrayTest {
+package ch05;
+
+class Car{
+    public int speed; // 속도
+    public int gear; // 기어
+    public String color; // 색상
+
+    public Car() {
+        speed = 0;
+        gear = 1;
+        color = "red";
+    }
+
+    public void speedUp() { // 속도 증가 메소드
+        speed += 10;
+    }
+
+    public String toString() {
+        return "속도: " + speed + " 기어: " + gear + "색상: " + color;
+    }
+}
+
+public class CarArrayTest {
+    public static void main(String[] args) {
+        final int NUM_cARS = 5;
+        Car[] cars = new Car[NUM_cARS];
+
+    }
 }
