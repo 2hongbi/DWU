2bfc93aa02f4c029fcf8a67af4b97918782c11e0
hongbi-lee <foxgirin950@gmail.com>
2022-03-31 18:17:43 +0900

    done with page 207

---
+++ dwu-oop/powerJava/src/ch06/CircleI.java
@@ -0,0 +1,8 @@
+package ch06;
+
+public class CircleI implements Drawable{ // p.204
+    int radius;
+    public void draw() {
+        System.out.println("Circle Draw");
+    }
+}
---
+++ dwu-oop/powerJava/src/ch06/TestInterFace1.java
@@ -0,0 +1,8 @@
+package ch06;
+
+public class TestInterFace1 {
+    public static void main(String[] args) {
+        Drawable obj = new CircleI();
+        obj.draw();
+    }
+}
