665a445b98da3bec7e1189cc882591708b631200
hongbi-lee <foxgirin950@gmail.com>
2022-05-05 22:06:21 +0900

    create Assignment9_2 file

--- dwu-oop/assignment/src/week9/Assignment9_2.java
+++ dwu-oop/assignment/src/week9/Assignment9_2.java
@@ -1,2 +1,23 @@
-package week9;public class Assignment9_2 {
+package week9;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Assignment9_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("한줄로 문장을 입력하세요. (종료는 exit을 입력) >> ");
+            String line = sc.nextLine();
+            if (line.equals("exit")) {
+                System.out.println("종료합니다...");
+                break;
+            }
+            StringTokenizer st = new StringTokenizer(line, " ");
+            int count = st.countTokens();
+            System.out.println("어절 개수는 " + count);
+            System.out.println("공백 개수는 " + (count - 1));
+        }
+        sc.close();
+    }
 }
