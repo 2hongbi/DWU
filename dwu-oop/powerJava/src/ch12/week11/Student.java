609c0eb84ca84e2e9d53ed1253241133150be10d
hongbi-lee <foxgirin950@gmail.com>
2022-05-13 11:11:12 +0900

    create week11 package and MapTest class

---
+++ dwu-oop/powerJava/src/ch12/week11/Student.java
@@ -0,0 +1,15 @@
+package ch12.week11;
+
+public class Student {
+    int number;
+    String name;
+
+    public Student (int number, String name) {
+        this.number =number;
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
