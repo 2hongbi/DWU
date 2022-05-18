1dd99bb27abdb4a3fccb29f41dffab3ae604310e
hongbi-lee <foxgirin950@gmail.com>
2022-05-18 23:53:34 +0900

    add Week11_1

--- dwu-oop/powerJava/src/ch12/week11/Week11_1.java
+++ dwu-oop/powerJava/src/ch12/week11/Week11_1.java
@@ -1,2 +1,27 @@
-package ch12.week11;public class Week11_1 {
+package ch12.week11;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Week11_1 {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+        studentMap.put("김길동", 85);
+        studentMap.put("홍길동", 90);
+        studentMap.put("최길동", 80);
+        studentMap.put("홍길동", 95);
+
+        System.out.println("총 Entry 수: " + studentMap.size());
+
+        for (Map.Entry<String, Integer> s : studentMap.entrySet()) {
+            String key = s.getKey();
+            Integer value = s.getValue();
+            System.out.println("    " + key + " : " + value);
+        }
+
+        Integer maxValue = Collections.max(studentMap.values());
+
+    }
 }
