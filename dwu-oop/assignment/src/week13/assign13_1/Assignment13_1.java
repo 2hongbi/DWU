b751b86ff2f1ee9e08bf69551f6788e5e6ce38ec
hongbi-lee <foxgirin950@gmail.com>
2022-05-24 18:54:48 +0900

    create Assignment13_1.java

--- dwu-oop/assignment/src/week13/assign13_1/Assignment13_1.java
+++ dwu-oop/assignment/src/week13/assign13_1/Assignment13_1.java
@@ -1,2 +1,18 @@
-package week13.assign13_1;public class Assignment13_1 {
+package week13.assign13_1;
+
+import java.io.*;
+import java.util.Locale;
+
+public class Assignment13_1 {
+    public static void main(String[] args){
+        try (BufferedReader in = new BufferedReader(new FileReader("/Users/isoyeon/Documents/dwu/DWU/dwu-oop/assignment/src/week13/assign13_1/in.txt"));
+             PrintWriter out = new PrintWriter(new FileWriter("/Users/isoyeon/Documents/dwu/DWU/dwu-oop/assignment/src/week13/assign13_1/out.txt"))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                out.println(line.toUpperCase());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
